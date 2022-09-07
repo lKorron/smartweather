bc554616ed7d76179fc9bfc6c2602d3b8e5d1818
Korron <lkorron@yandex.ru>
2022-09-07 13:04:44 +0300

    add new weather parameters

--- src/components/WeatherPresentation.vue
+++ src/components/WeatherPresentation.vue
@@ -11,24 +11,44 @@
       <div class="presentation__temp temp">
         <h2 class="temp__header">Показатели темературы</h2>
         <div class="temp__average">
-          <div class="temp__name">Средняя температура</div>
+          <div class="temp__name">Средняя температура:</div>
           <b>{{ getCelsiusTemp(cityCard.data?.main.temp) }}°C</b>
         </div>
         <div class="temp__feelslike">
-          <div class="temp__name">Ощущается как</div>
+          <div class="temp__name">Ощущается как:</div>
           <b>{{ getCelsiusTemp(cityCard.data?.main.feels_like, 100) }}°C</b>
         </div>
         <div class="temp__min">
-          <div class="temp__name">Минимальная температура</div>
+          <div class="temp__name">Минимальная температура:</div>
           <b>{{ getCelsiusTemp(cityCard.data?.main.temp_min, 100) }}°C</b>
         </div>
         <div class="temp__max">
-          <div class="temp__name">Максимальная температура</div>
+          <div class="temp__name">Максимальная температура:</div>
           <b>{{ getCelsiusTemp(cityCard.data?.main.temp_max, 100) }}°C</b>
         </div>
       </div>
       <div class="presentation__other other">
-        <h2 class="other__head">Другие показатели</h2>
+        <h2 class="other__header">Другие показатели</h2>
+        <div class="other__pressure">
+          <div class="other__name">Давление:</div>
+          <b>{{ getMmPressure(cityCard.data?.main.pressure) }} мм.рт.ст.</b>
+        </div>
+        <div class="other__humidity">
+          <div class="other__name">Влажность:</div>
+          <b>{{ getMmPressure(cityCard.data?.main.humidity) }}%</b>
+        </div>
+        <div class="other__wind">
+          <div class="other__name">Скорость ветра:</div>
+          <b>{{ getMmPressure(cityCard.data?.wind.speed) }}м/c</b>
+        </div>
+        <div v-if="cityCard.data?.wind.gust" class="other__gust">
+          <div class="other__name">Скорость порывов ветра:</div>
+          <b>{{ getMmPressure(cityCard.data?.wind.gust) }}м/c</b>
+        </div>
+        <div class="other__wind-direction">
+          <div class="other__name">Направление ветра:</div>
+          <b>{{ getMmPressure(cityCard.data?.wind.deg) }}°</b>
+        </div>
       </div>
     </div>
   </div>
@@ -47,6 +67,12 @@ export default {
     getCelsiusTemp(kelvinTemp, acc = 10) {
       if (kelvinTemp) {
         return Math.round((kelvinTemp - 273.15) * acc) / acc;
+      }
+      return "";
+    },
+    getMmPressure(hpaPressure, acc = 10) {
+      if (hpaPressure) {
+        return Math.round((hpaPressure / 1.333) * acc) / acc;
       }
       return "";
     },
@@ -92,12 +118,20 @@ export default {
   padding: 10px;
 }
 
-.presentation__temp {
+.presentation__temp,
+.presentation__other {
   > div {
     display: flex;
+    justify-content: center;
   }
 
-  .temp__name {
+  .temp__header,
+  .other__header {
+    font-size: 18px;
+  }
+
+  .temp__name,
+  .other__name {
     margin-right: 10px;
   }
 }
