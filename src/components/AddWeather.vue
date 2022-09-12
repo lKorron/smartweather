bbfb7c5b0b301a1a2c9c3248a170d2971d29de4e
Korron <lkorron@yandex.ru>
2022-09-12 20:19:45 +0300

    addWeather component added

---
+++ src/components/AddWeather.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="input-container">
+    <label for="city-input">Введите название города</label>
+    <input
+      ref="input"
+      @keydown.enter="addCity"
+      v-model="inputData"
+      id="city-input"
+      type="text"
+    />
+    <button class="add-button" @click="addCity">Добавить</button>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: {
+    "add-city": (value) => typeof value === "string",
+  },
+
+  data() {
+    return {
+      inputData: "",
+    };
+  },
+  methods: {
+    addCity() {
+      if (this.inputData === "") {
+        alert("Введите название города!");
+        return;
+      }
+      this.$emit("add-city", this.inputData);
+    },
+  },
+};
+</script>
+
+<style></style>
