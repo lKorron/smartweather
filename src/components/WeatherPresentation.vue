fb065f38a66a765caf9cef7731ed52e22ecabb53
Korron <lkorron@yandex.ru>
2022-09-16 17:09:12 +0300

    visual added

--- src/components/WeatherPresentation.vue
+++ src/components/WeatherPresentation.vue
@@ -1,63 +1,65 @@
 <template>
   <div class="presentation">
-    <!-- <div class="presentation__name">{{ cityCard.name }}</div>
-    <div class="presentation__image">
-      <img :src="iconSrc" alt="preview image" />
-    </div>
-    <div class="card__description">
-      {{ description }}
-    </div> -->
     <div class="presentation__body">
-      <div class="presentation__temp temp">
-        <h2 class="temp__header">Показатели темературы</h2>
-        <div class="temp__average">
-          <div class="temp__name">Средняя температура:</div>
-          <b>{{ getCelsiusTemp(cityCard.data?.main.temp) }}°C</b>
+      <div class="presentation__data">
+        <div class="presentation__temp temp">
+          <h2 class="temp__header">Показатели темературы</h2>
+          <div class="temp__average">
+            <div class="temp__name">Средняя температура:</div>
+            <b>{{ getCelsiusTemp(cityCard.data?.main.temp) }}°C</b>
+          </div>
+          <div class="temp__feelslike">
+            <div class="temp__name">Ощущается как:</div>
+            <b>{{ getCelsiusTemp(cityCard.data?.main.feels_like, 100) }}°C</b>
+          </div>
+          <div class="temp__min">
+            <div class="temp__name">Минимальная температура:</div>
+            <b>{{ getCelsiusTemp(cityCard.data?.main.temp_min, 100) }}°C</b>
+          </div>
+          <div class="temp__max">
+            <div class="temp__name">Максимальная температура:</div>
+            <b>{{ getCelsiusTemp(cityCard.data?.main.temp_max, 100) }}°C</b>
+          </div>
         </div>
-        <div class="temp__feelslike">
-          <div class="temp__name">Ощущается как:</div>
-          <b>{{ getCelsiusTemp(cityCard.data?.main.feels_like, 100) }}°C</b>
-        </div>
-        <div class="temp__min">
-          <div class="temp__name">Минимальная температура:</div>
-          <b>{{ getCelsiusTemp(cityCard.data?.main.temp_min, 100) }}°C</b>
-        </div>
-        <div class="temp__max">
-          <div class="temp__name">Максимальная температура:</div>
-          <b>{{ getCelsiusTemp(cityCard.data?.main.temp_max, 100) }}°C</b>
+        <div class="presentation__other other">
+          <h2 class="other__header">Другие показатели</h2>
+          <div class="other__pressure">
+            <div class="other__name">Давление:</div>
+            <b>{{ getMmPressure(cityCard.data?.main.pressure) }} мм.рт.ст.</b>
+          </div>
+          <div class="other__humidity">
+            <div class="other__name">Влажность:</div>
+            <b>{{ getMmPressure(cityCard.data?.main.humidity) }} %</b>
+          </div>
+          <div class="other__wind">
+            <div class="other__name">Скорость ветра:</div>
+            <b>{{ getMmPressure(cityCard.data?.wind.speed) }} м/c</b>
+          </div>
+          <div v-if="cityCard.data?.wind.gust" class="other__gust">
+            <div class="other__name">Скорость порывов ветра:</div>
+            <b>{{ getMmPressure(cityCard.data?.wind.gust) }} м/c</b>
+          </div>
+          <div class="other__wind-direction">
+            <div class="other__name">Направление ветра:</div>
+            <b>{{ getMmPressure(cityCard.data?.wind.deg) }}°</b>
+          </div>
         </div>
       </div>
-      <div class="presentation__other other">
-        <h2 class="other__header">Другие показатели</h2>
-        <div class="other__pressure">
-          <div class="other__name">Давление:</div>
-          <b>{{ getMmPressure(cityCard.data?.main.pressure) }} мм.рт.ст.</b>
-        </div>
-        <div class="other__humidity">
-          <div class="other__name">Влажность:</div>
-          <b>{{ getMmPressure(cityCard.data?.main.humidity) }} %</b>
-        </div>
-        <div class="other__wind">
-          <div class="other__name">Скорость ветра:</div>
-          <b>{{ getMmPressure(cityCard.data?.wind.speed) }} м/c</b>
-        </div>
-        <div v-if="cityCard.data?.wind.gust" class="other__gust">
-          <div class="other__name">Скорость порывов ветра:</div>
-          <b>{{ getMmPressure(cityCard.data?.wind.gust) }} м/c</b>
-        </div>
-        <div class="other__wind-direction">
-          <div class="other__name">Направление ветра:</div>
-          <b>{{ getMmPressure(cityCard.data?.wind.deg) }}°</b>
-        </div>
-      </div>
+      <wind-compass></wind-compass>
     </div>
   </div>
 </template>
 
 <script>
+import WindCompass from "../components/WindCompass.vue";
 import { capitalizeFirstLetter } from "../formatMethods.js";
 export default {
   name: "WeatherPresentation",
+
+  components: {
+    WindCompass,
+  },
+
   props: { cityCard: Object },
   data() {
     return {};
@@ -104,7 +106,7 @@ export default {
   margin-top: 20px;
   box-shadow: 3px 3px 2px gray;
 
-  &__body {
+  &__data {
     display: grid;
     grid-template-columns: 1fr 1fr;
   }
---
+++ src/components/WindCompass.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="compass">
+    <img class="compass__image" src="../assets/compass.png" alt="compass" />
+    <div class="compass__arrow">
+      <img src="../assets/compass-arrow.png" alt="compass" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      rotationAngle: 30,
+    };
+  },
+
+  methods: {
+    rotateArrow(deg) {
+      this.rotationAngle = deg;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../scss/mixins.scss";
+
+.compass {
+  @include sizeImgContainer($size: 300px);
+  position: relative;
+  margin: 0 auto;
+  border: 1px dashed gray;
+  border-radius: 10px;
+  padding: 20px;
+  margin-top: 10px;
+  margin-bottom: 20px;
+}
+
+.compass__arrow {
+  @include sizeImgContainer($size: 80px);
+  position: absolute;
+  margin: auto;
+  top: 0;
+  right: 0;
+  bottom: 3px;
+  left: 15px;
+  text-align: center;
+  transition: transform 0.2s;
+
+  &:hover {
+    transform: rotate(-21.8deg);
+  }
+}
+</style>
